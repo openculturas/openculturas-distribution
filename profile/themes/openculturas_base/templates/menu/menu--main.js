df4f240ad79ea80fa9b3289b6914164488d879d0
Peter Gerken <peter.gerken@erdfisch.de>
2022-02-16 17:30:00 +0100

    Offcanvas menu styling and logic

---
+++ profile/themes/openculturas_base/templates/menu/menu--main.js
@@ -0,0 +1,36 @@
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Opening / Closing logic for the main menu inside of offcanvas menu.
+   */
+  Drupal.behaviors.mainMenu = {
+    attach: function (context, settings) {
+      $('.menu-item--toggle', context).once('init-main-menu').click(this.toggleMenuItem);
+      // Toggle initial states.
+      $('.menu--main ul').each(function() {
+        if ($(this).parent().hasClass('menu-item--active-trail')) {
+          $(this).slideDown();
+          $(this).parent().addClass('menu-item--open');
+        } else {
+          if (!$(this).parent().hasClass('menu--main')) {
+            $(this).slideUp();
+          }
+        }
+      });
+    },
+    toggleMenuItem: function(e= null) {
+      let $menuItem = $(this).parent();
+      let $allMenuItems = $('.menu--main li.menu-item');
+      let wasOpen = $menuItem.hasClass('menu-item--open');
+
+      if (wasOpen) {
+        $menuItem.removeClass('menu-item--open').children('ul').slideUp();
+      } else {
+        $menuItem.siblings().removeClass('menu-item--open').children('ul').slideUp();
+        $menuItem.addClass('menu-item--open').children('ul').slideDown();
+      }
+    }
+  };
+} (jQuery, Drupal));
