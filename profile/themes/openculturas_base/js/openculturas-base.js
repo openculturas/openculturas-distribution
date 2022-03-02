13f78c3c81d49247d84e015c7ee7062113a5a5ca
Peter Gerken <peter.gerken@erdfisch.de>
2022-03-02 23:56:53 +0100

    Theming: Teaser styling

--- profile/themes/openculturas_base/js/openculturas-base.js
+++ profile/themes/openculturas_base/js/openculturas-base.js
@@ -66,23 +66,3 @@
     }
   };
 } (jQuery, Drupal));
-
-/**
- * @file
- * OpenCulturas Base behaviors.
- */
-
-(function ($, Drupal) {
-
-  'use strict';
-
-  /**
-   * Behavior description.
-   */
-  Drupal.behaviors.nodeLocation = {
-    attach: function (context, settings) {
-
-    }
-  };
-} (jQuery, Drupal));
-
