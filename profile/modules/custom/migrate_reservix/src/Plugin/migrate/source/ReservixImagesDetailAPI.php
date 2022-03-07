d2b430a8a99669e4661374727ffa88e0ec0ee3f0
sanduhrs <stefan@auditor.email>
2022-03-08 00:28:21 +0100

    Add reservix image migration

---
+++ profile/modules/custom/migrate_reservix/src/Plugin/migrate/source/ReservixImagesDetailAPI.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\migrate_reservix\Plugin\migrate\source;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\migrate\Row;
+
+/**
+ * Provides a flexible, generic import source for the Reservix API.
+ *
+ * @MigrateSource(
+ *   id = "migrate_reservix_images_detail_source",
+ *   label = @Translation("Reservix Images Detail API"),
+ * )
+ */
+class ReservixImagesDetailAPI extends ReservixBaseAPI {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state): array {
+    $form = parent::buildConfigurationForm($form, $form_state);
+    $form['endpoint']['#value'] = ['custom/imagesdetail'];
+    $form['endpoint']['#attributes']['disabled'] = 'disabled';
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row): bool {
+    // Always include this fragment at the beginning of every prepareRow()
+    // implementation, so parent classes can ignore rows.
+    if (parent::prepareRow($row) === FALSE) {
+      return FALSE;
+    }
+    return TRUE;
+  }
+
+}
