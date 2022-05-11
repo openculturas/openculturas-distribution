117b6016235a14ddf47f3452511377fa9ca393a5
Thorsten Krug <tho@donkeymedia.eu>
2022-05-11 19:48:08 +0200

    Flaggs,Events,Mybookmarks styles only.

---
+++ profile/themes/openculturas_base/templates/page/ajax-progress-throbber.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * Replaced Drupal cores ajax throbber(s), see: https://www.drupal.org/node/2974681
+ *
+ */
+(function ($, Drupal) {
+  Drupal.theme.ajaxProgressThrobber = function () {
+    return "<div class=\"ajax-spinner ajax-spinner--inline\"><span class=\"ajax-spinner__label\">" + Drupal.t('Loading&nbsp;&hellip;', {}, {
+      context: "Loading text for Drupal cores Ajax throbber (inline)"
+    }) + "</span></div>";
+  };
+
+  Drupal.theme.ajaxProgressIndicatorFullscreen = function () {
+    return "<div class=\"ajax-spinner ajax-spinner--fullscreen\"><span class=\"ajax-spinner__label\">" + Drupal.t('Loading&nbsp;&hellip;', {}, {
+      context: "Loading text for Drupal cores Ajax throbber (fullscreen)"
+    }) + "</span></div>";
+  };
+  // You can also customize only throbber message:
+  // Drupal.theme.ajaxProgressMessage = message => '<div class="my-message">' + message + '</div>';
+})(jQuery, Drupal);
