a5c5465286a343d64a9948b3e8c79f9710e5f16c
tobiasb <tobias.baehr@gzevd.de>
2022-05-12 14:07:08 +0200

    User: configure user edit/register form

---
+++ profile/modules/custom/openculturas_custom/src/Routing/RouteSubscriber.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\openculturas_custom\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    // Display account edit in frontend theme.
+    if ($route = $collection->get('entity.user.edit_form')) {
+      $route->setOption('_admin_route', FALSE);
+    }
+    if ($route = $collection->get('entity.user.cancel_form')) {
+      $route->setOption('_admin_route', FALSE);
+    }
+  }
+
+}
