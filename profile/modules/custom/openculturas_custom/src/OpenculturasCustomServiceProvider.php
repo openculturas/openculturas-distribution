a16c7d7ca3f2f9aa3670ac5eff5902d6e6c51cd0
tobiasb <tobias.baehr@gzevd.de>
2022-07-29 13:40:53 +0200

    Display a geo link under map
    Issue: lvsn/openculturas#533

--- profile/modules/custom/openculturas_custom/src/OpenculturasCustomServiceProvider.php
+++ profile/modules/custom/openculturas_custom/src/OpenculturasCustomServiceProvider.php
@@ -8,6 +8,7 @@ use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
+use function is_array;
 
 /**
  * Service Provider for Openculturas
@@ -18,6 +19,12 @@ class OpenculturasCustomServiceProvider extends ServiceProviderBase {
    * {@inheritdoc}
    */
   public function alter(ContainerBuilder $container) {
+    $filter_protocols = $container->getParameter('filter_protocols');
+    if (is_array($filter_protocols)) {
+      $filter_protocols[] = 'geo';
+      $container->setParameter('filter_protocols', $filter_protocols);
+    }
+
     $modules = $container->getParameter('container.modules');
     if (isset($modules['default_content'])) {
       $service_definition = new Definition('Drupal\openculturas_custom\Normalizer', array(
