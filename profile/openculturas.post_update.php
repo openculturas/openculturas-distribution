7102596f0ce222460da2b99872240d4166ee554a
tobiasb <tobias.baehr@gzevd.de>
2022-12-06 18:28:59 +0100

    fix: Do not show dates from earlier dates on same day

--- profile/openculturas.post_update.php
+++ profile/openculturas.post_update.php
@@ -35,3 +35,17 @@ function openculturas_post_update_0001() {
   // Output logged messages to related channel of update execution.
   return $updater->logger()->output();
 }
+
+/**
+ * Views: Set granularity to hour for the not exposed start/end date filter.
+ */
+function openculturas_post_update_0002() {
+  /** @var \Drupal\update_helper\Updater $updater */
+  $updater = \Drupal::service('update_helper.updater');
+
+  // Execute configuration update definitions with logging of success.
+  $updater->executeUpdate('openculturas', 'openculturas_post_update_0002');
+
+  // Output logged messages to related channel of update execution.
+  return $updater->logger()->output();
+}
