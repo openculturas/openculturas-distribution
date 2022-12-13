db8430421ab7fdee5785122b9cd92e022f980675
tobiasb <tobias.baehr@gzevd.de>
2022-12-13 10:56:41 +0100

    fix: missing unpublish moderation state option in bulk edit

--- profile/openculturas.post_update.php
+++ profile/openculturas.post_update.php
@@ -49,3 +49,17 @@ function openculturas_post_update_0002() {
   // Output logged messages to related channel of update execution.
   return $updater->logger()->output();
 }
+
+/**
+ * Adds missing unpublish moderation state option in bulk edit
+ */
+function openculturas_post_update_0003() {
+  /** @var \Drupal\update_helper\Updater $updater */
+  $updater = \Drupal::service('update_helper.updater');
+
+  // Execute configuration update definitions with logging of success.
+  $updater->executeUpdate('openculturas', 'openculturas_post_update_0003');
+
+  // Output logged messages to related channel of update execution.
+  return $updater->logger()->output();
+}
