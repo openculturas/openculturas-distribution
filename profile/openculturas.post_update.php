159206f05f59a44ac4f04b1a8976c0cc054c90e9
tobiasb <tobias.baehr@gzevd.de>
2023-04-27 11:18:31 +0200

    fix(dashboard): Limit type filter options for my content table

--- profile/openculturas.post_update.php
+++ profile/openculturas.post_update.php
@@ -394,3 +394,17 @@ function openculturas_post_update_0012(): string {
   // Output logged messages to related channel of update execution.
   return $updater->logger()->output();
 }
+
+/**
+ * Limit type filter options for my content table.
+ */
+function openculturas_post_update_0013(): string {
+  /** @var \Drupal\update_helper\Updater $updater */
+  $updater = \Drupal::service('update_helper.updater');
+
+  // Execute configuration update definitions with logging of success.
+  $updater->executeUpdate('openculturas', 'openculturas_post_update_0013');
+
+  // Output logged messages to related channel of update execution.
+  return $updater->logger()->output();
+}
