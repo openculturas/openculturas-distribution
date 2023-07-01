423e65bbed3239e07d6e44de03a6225e83f1e09c
tobiasb <tobias.baehr@gzevd.de>
2023-07-01 18:07:58 +0200

    refactor: Switch to "modern" js

--- scripts/js/log.js
+++
@@ -1,5 +0,0 @@
-export default function log(message) {
-  // Logging human-readable timestamp.
-  // eslint-disable-next-line no-console
-  console.log(`[${new Date().toTimeString().slice(0, 8)}] ${message}`);
-}
