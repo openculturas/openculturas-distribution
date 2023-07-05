2500929a2a35a78b0543cfa96408019e15d88288
tobiasb <tobias.baehr@gzevd.de>
2023-07-05 10:05:09 +0200

    fix: Replace output of field_address_location in upcoming dates map

--- profile/openculturas.post_update.php
+++ profile/openculturas.post_update.php
@@ -454,3 +454,17 @@ function openculturas_post_update_0016(): string {
   // Output logged messages to related channel of update execution.
   return $updater->logger()->output();
 }
+
+/**
+ * Configuration update.
+ */
+function openculturas_post_update_0017(): string {
+  /** @var \Drupal\update_helper\Updater $updater */
+  $updater = \Drupal::service('update_helper.updater');
+
+  // Execute configuration update definitions with logging of success.
+  $updater->executeUpdate('openculturas', 'openculturas_post_update_0017');
+
+  // Output logged messages to related channel of update execution.
+  return $updater->logger()->output();
+}
