d3151c28f030018f9b3c3da21cc5bf7d0ff44cfd
Meike Jung <meike.jung@cowain.de>
2023-07-25 14:25:46 +0200

    feat: Enable mastodon in contact data paragraph
    Co-authored-by: tobiasb <tobias.baehr@gzevd.de>

--- profile/openculturas.post_update.php
+++ profile/openculturas.post_update.php
@@ -493,3 +493,17 @@ function openculturas_post_update_0018(?array &$sandbox = NULL): void {
 
   $config_entity_updater->update($sandbox, 'entity_view_display', $callback);
 }
+
+/**
+ * Enable mastodon.
+ */
+function openculturas_post_update_0019(): string {
+  /** @var \Drupal\update_helper\Updater $updater */
+  $updater = \Drupal::service('update_helper.updater');
+
+  // Execute configuration update definitions with logging of success.
+  $updater->executeUpdate('openculturas', 'openculturas_post_update_0019');
+
+  // Output logged messages to related channel of update execution.
+  return $updater->logger()->output();
+}
