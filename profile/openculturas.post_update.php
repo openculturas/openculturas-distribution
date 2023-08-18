72b08773a6e763f0ff9f049a27e089f7bbbd6213
tobiasb <tobias.baehr@gzevd.de>
2023-08-18 16:09:47 +0200

    feat: Index type taxonomies

--- profile/openculturas.post_update.php
+++ profile/openculturas.post_update.php
@@ -1072,3 +1072,17 @@ function openculturas_post_update_0031(): string {
   // Output logged messages to related channel of update execution.
   return $updater->logger()->output();
 }
+
+/**
+ * Index type taxonomies.
+ */
+function openculturas_post_update_0032(): string {
+  /** @var \Drupal\update_helper\Updater $updater */
+  $updater = \Drupal::service('update_helper.updater');
+
+  // Execute configuration update definitions with logging of success.
+  $updater->executeUpdate('openculturas', 'openculturas_post_update_0032');
+
+  // Output logged messages to related channel of update execution.
+  return $updater->logger()->output();
+}
