436bd16c554d9472fbad91fac54b77c9729f7015
tobiasb <tobias.baehr@gzevd.de>
2023-08-30 15:53:15 +0200

    fix: Enable translation of teaser

--- profile/openculturas.post_update.php
+++ profile/openculturas.post_update.php
@@ -1179,3 +1179,17 @@ function openculturas_post_update_0037(): string {
   // Output logged messages to related channel of update execution.
   return $updater->logger()->output();
 }
+
+/**
+ * Enable translation of teaser.
+ */
+function openculturas_post_update_0038(): string {
+  /** @var \Drupal\update_helper\Updater $updater */
+  $updater = \Drupal::service('update_helper.updater');
+
+  // Execute configuration update definitions with logging of success.
+  $updater->executeUpdate('openculturas', 'openculturas_post_update_0038');
+
+  // Output logged messages to related channel of update execution.
+  return $updater->logger()->output();
+}
