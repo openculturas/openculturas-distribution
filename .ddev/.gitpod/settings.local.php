b12280889990fa06817d84e74b79ffd3c0eec5bc
tobiasb <tobias.baehr@gzevd.de>
2023-09-14 16:20:41 +0200

    chore: Fix gitpod config

--- .ddev/.gitpod/settings.local.php
+++ .ddev/.gitpod/settings.local.php
@@ -2,3 +2,5 @@
 
 // somehow ddev can not resolve the db service with service name.
 $databases['default']['default']['host'] = 'ddev-openculturas-db';
+
+// $config['stage_file_proxy.settings']['origin'] = 'https://example.org';
