924a6ca09a097f1d9d219c1bd869851c6adcceaa
tobiasb <tobias.baehr@gzevd.de>
2023-09-22 12:35:35 +0200

    fix: Duplicated profiles was shown in member-of block

--- profile/openculturas.post_update.php
+++ profile/openculturas.post_update.php
@@ -1356,3 +1356,17 @@ function openculturas_post_update_0044(): void {
     }
   }
 }
+
+/**
+ * Configuration update.
+ */
+function openculturas_post_update_0045(): string {
+  /** @var \Drupal\update_helper\Updater $updater */
+  $updater = \Drupal::service('update_helper.updater');
+
+  // Execute configuration update definitions with logging of success.
+  $updater->executeUpdate('openculturas', 'openculturas_post_update_0045');
+
+  // Output logged messages to related channel of update execution.
+  return $updater->logger()->output();
+}
