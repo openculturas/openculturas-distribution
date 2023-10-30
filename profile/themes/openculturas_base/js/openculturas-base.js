050de3e8604b70d1b69ded2a47c31c24e6b49207
Alena Hackradt <alena@hackradt.com>
2023-10-30 17:50:06 +0100

    feat: offcanvas-menu as dialog-element with optimized closing-logic

--- profile/themes/openculturas_base/js/openculturas-base.js
+++ profile/themes/openculturas_base/js/openculturas-base.js
@@ -189,38 +189,99 @@
   // Drupal.theme.ajaxProgressMessage = message => '<div class="my-message">' + message + '</div>';
 })(jQuery, Drupal);
 
-(function ($, Drupal) {
+(function (Drupal, once) {
 
-  'use strict';
 
   /**
    * Opening / Closing logic for menu toggles (e. g. offcanvas burger menu).
    */
   Drupal.behaviors.pageHeaderOffcanvasMenu = {
     attach: function (context, settings) {
-      $('.header__burger--buttons', context).once('init-offcanvas').click(this.toggleOffcanvasMenu);
-    },
-    toggleOffcanvasMenu: function(e= null) {
-      let $body = $('body');
-      let $offcanvasMenu = $('#offcanvas_menu');
-      let $burgerButtons = $('.header__burger--buttons');
-      let wasOpen = $body.hasClass('offcanvas-open');
 
-      $body.toggleClass('offcanvas-open');
-
-      // Accessibility tweaks.
-      $burgerButtons.each(function() {
-        $(this).attr('aria-expanded', !($(this).attr('aria-expanded') === 'true'));
+      // Adding an eventlistener on the BurgerMenu-Button on click
+      once('init-offcanvas', '.header__burger--buttons', context).forEach((headerBurgerButton) => {
+        headerBurgerButton.addEventListener('click', (event) => {
+          this.toggleOffcanvasMenu();
+        })
       });
-      $offcanvasMenu.attr('aria-hidden', !($offcanvasMenu.attr('aria-hidden') === 'true'));
 
+      // Adding an eventlistener on the body on click
+      document.body.addEventListener('click', (event) => {
+        this.menuDialogBackdrop(event);
+      })
+      document.addEventListener('keydown', (event) => {
+        this.menuDialogKeypress(event);
+      })
+    },
+    // Check whether event target (click) is neither in a button or dialog element
+    menuDialogBackdrop: function (event) {
+      // Abort if no event was found
+      if(!event) {
+        return;
+      }
+
+      const isBackdrop = !(event.target.closest('.header__offcanvas-menu--grid')) && !(event.target.closest('.header__burger--buttons'));
+
+      if(isBackdrop) {
+        // Event target (click) is neither in a button or dialog element => cleanup dialogs
+        this.menuDialogCleanup();
+      }
+    },
+    // Close the menu and set atrributes (aria and dialog)
+    menuDialogCleanup: function () {
+      const burgerButtonOpen = document.querySelector('#button-offcanvas-open');
+      const burgerButtonClose = document.querySelector('#button-offcanvas-close');
+      const offcanvasMenu = document.querySelector('#offcanvas_menu');
+      burgerButtonOpen.setAttribute('aria-expanded', 'false');
+      burgerButtonClose.setAttribute('aria-expanded', 'false');
+      offcanvasMenu.setAttribute('aria-hidden', 'true')
+      // Close all open dialogs
+      document.body.classList.remove('offcanvas-open');
+
+      // set timeout so the menu slides out
+        setTimeout(() => {
+          document.querySelector('#offcanvas_menu_dialog' + '[open]')?.close();
+        }, 150);
+    },
+    // Toggle the BurgerMenu-Button and aria-labels
+    toggleOffcanvasMenu: function() {
+      const offcanvasMenuDialog = document.querySelector('#offcanvas_menu_dialog');
+      const burgerButtonOpen = document.querySelector('#button-offcanvas-open');
+      const offcanvasMenu = document.querySelector('#offcanvas_menu');
+      const burgerButtonClose = document.querySelector('#button-offcanvas-close');
+      const wasOpen = document.body.classList.contains('offcanvas-open');
+      const offcanvasMenuDialogOpen = offcanvasMenuDialog.hasAttribute('open');
+
+      if (!offcanvasMenuDialogOpen) {
+        burgerButtonOpen.setAttribute('aria-expanded', 'true');
+        burgerButtonClose.setAttribute('aria-expanded', 'true');
+        offcanvasMenu.setAttribute('aria-hidden', 'false')
+        offcanvasMenuDialog.setAttribute('open', '');
+        setTimeout(() => {
+          document.body.classList.add('offcanvas-open');
+        }, 150);
+      } else {
+        this.menuDialogCleanup();
+      }
+
+      // Set focus in menu when its open
       if (!wasOpen) {
-        $('.header__offcanvas-menu--content').children().eq(0).focus();
+        const offcanvasMenuContent = document.querySelector('.header__offcanvas-menu--content')
+        offcanvasMenuContent.firstElementChild.focus();
+      }
+    },
+    // close menu on ESC
+    menuDialogKeypress: function (event) {
+      if (!event || !event.key) {
+          return;
+      }
+      const keyName = event.key;
+      if(keyName === 'Escape') {
+          this.menuDialogCleanup();
       }
     }
-  };
-
-} (jQuery, Drupal));
+  }
+} (Drupal, once));
 
 (function ($) {
 
