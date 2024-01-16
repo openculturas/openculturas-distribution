5aad9e17e0f4c923daa285822e1b8e4e9aa7e7bc
Meike Jung <meike.jung@cowain.de>
2024-01-16 18:32:26 +0100

    Issue #3414972: Provide sensible OC configuration with CKEditor5 update

--- profile/themes/openculturas_base/js/openculturas-base.js
+++ profile/themes/openculturas_base/js/openculturas-base.js
@@ -131,43 +131,6 @@
     };
   
   } (Drupal, once));
-(function ($, Drupal, once) {
-
-  'use strict';
-
-  /**
-   * Opening / Closing logic for the main menu inside of offcanvas menu.
-   */
-  Drupal.behaviors.mainMenu = {
-    attach: function (context, settings) {
-      $(once('init-main-menu', '.menu-item--toggle', context)).click(this.toggleMenuItem);
-      // Toggle initial states.
-      $('.region-offcanvas-menu ul').each(function() {
-        if ($(this).parent().hasClass('menu-item--active-trail')) {
-          $(this).slideDown();
-          $(this).parent().addClass('menu-item--open');
-        } else {
-          if (!$(this).parent().hasClass('block-menu')) {
-            $(this).slideUp();
-          }
-        }
-      });
-    },
-    toggleMenuItem: function() {
-      let $menuItem = $(this).parent();
-      let $allMenuItems = $('.region-offcanvas-menu li.menu-item');
-      let wasOpen = $menuItem.hasClass('menu-item--open');
-
-      if (wasOpen) {
-        $menuItem.removeClass('menu-item--open').children('ul').slideUp();
-      } else {
-        $menuItem.siblings().removeClass('menu-item--open').children('ul').slideUp();
-        $menuItem.addClass('menu-item--open').children('ul').slideDown();
-      }
-    }
-  };
-} (jQuery, Drupal, once));
-
 /**
  * @file
  * Replaced Drupal cores ajax throbber(s), see: https://www.drupal.org/node/2974681
@@ -320,3 +283,40 @@
     },
   };
 }(jQuery, Drupal, once));
+
+(function ($, Drupal, once) {
+
+  'use strict';
+
+  /**
+   * Opening / Closing logic for the main menu inside of offcanvas menu.
+   */
+  Drupal.behaviors.mainMenu = {
+    attach: function (context, settings) {
+      $(once('init-main-menu', '.menu-item--toggle', context)).click(this.toggleMenuItem);
+      // Toggle initial states.
+      $('.region-offcanvas-menu ul').each(function() {
+        if ($(this).parent().hasClass('menu-item--active-trail')) {
+          $(this).slideDown();
+          $(this).parent().addClass('menu-item--open');
+        } else {
+          if (!$(this).parent().hasClass('block-menu')) {
+            $(this).slideUp();
+          }
+        }
+      });
+    },
+    toggleMenuItem: function() {
+      let $menuItem = $(this).parent();
+      let $allMenuItems = $('.region-offcanvas-menu li.menu-item');
+      let wasOpen = $menuItem.hasClass('menu-item--open');
+
+      if (wasOpen) {
+        $menuItem.removeClass('menu-item--open').children('ul').slideUp();
+      } else {
+        $menuItem.siblings().removeClass('menu-item--open').children('ul').slideUp();
+        $menuItem.addClass('menu-item--open').children('ul').slideDown();
+      }
+    }
+  };
+} (jQuery, Drupal, once));
