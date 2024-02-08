221fab85cb755a7990a280a2b5c1a72589a335c5
Alena Hackradt <alena@hackradt.com>
2024-02-08 12:47:51 +0100

    feat: flag hint: fallback buttons for anonymous users
    Co-authored-by: tobiasb <tobias.baehr@gzevd.de>
    Co-authored-by: Alena Hackradt <alena.hackradt@gzevd.de>

--- profile/modules/custom/openculturas_custom/src/Plugin/Block/BookmarkFlagIconBlock.php
+++ profile/modules/custom/openculturas_custom/src/Plugin/Block/BookmarkFlagIconBlock.php
@@ -7,7 +7,10 @@ namespace Drupal\openculturas_custom\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\UncacheableDependencyTrait;
 use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\openculturas_custom\CurrentEntityHelper;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a bookmark flag icon block.
@@ -17,9 +20,23 @@ use Drupal\openculturas_custom\CurrentEntityHelper;
  *   admin_label = @Translation("Bookmark flag icon")
  * )
  */
-final class BookmarkFlagIconBlock extends BlockBase {
+final class BookmarkFlagIconBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   use UncacheableDependencyTrait;
+
+  /**
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected AccountInterface $account;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): BookmarkFlagIconBlock {
+    $instance = new self($configuration, $plugin_id, $plugin_definition);
+    $instance->account = $container->get('current_user');
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
@@ -32,16 +49,27 @@ final class BookmarkFlagIconBlock extends BlockBase {
       && ($current_entity->getEntityTypeId() === 'node'
         || $current_entity->getEntityTypeId() === 'taxonomy_term')
       ) {
-      $build['flag_bookmark_' . $current_entity->getEntityTypeId()] = [
-        '#lazy_builder' => [
-          'flag.link_builder:build', [
-            $current_entity->getEntityTypeId(),
-            $current_entity->id(),
-            ($current_entity->getEntityTypeId() === 'taxonomy_term') ? 'bookmark_term' : 'bookmark_node',
+      if ($this->account->isAuthenticated()) {
+        $build['flag_bookmark_' . $current_entity->getEntityTypeId()] = [
+          '#lazy_builder' => [
+            'flag.link_builder:build', [
+              $current_entity->getEntityTypeId(),
+              $current_entity->id(),
+              ($current_entity->getEntityTypeId() === 'taxonomy_term') ? 'bookmark_term' : 'bookmark_node',
+            ],
           ],
-        ],
-        '#create_placeholder' => TRUE,
-      ];
+          '#create_placeholder' => TRUE,
+        ];
+      }
+      else {
+        $build['flag_bookmark_' . $current_entity->getEntityTypeId()] = [
+          '#type' => 'inline_template',
+          '#template' => <<<EOF
+<div class="flag flag-bookmark-node action-flag"><a class="flag--link disabled" href="{{ path('user.login',{},{'query':{'destination': path('<current>') }}) }}" title="{{ 'Log in to bookmark' | t}}"></a></div>
+EOF
+        ];
+      }
+
       return $build;
     }
 
