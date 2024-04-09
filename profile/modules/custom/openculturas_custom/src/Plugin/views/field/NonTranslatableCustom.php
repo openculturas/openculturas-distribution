eddf6f495c845028d40ee5ca481f2690ce27c18d
tobiasb <tobias.baehr@gzevd.de>
2024-04-09 18:25:14 +0200

    fix: Use a custom view handler for markup

---
+++ profile/modules/custom/openculturas_custom/src/Plugin/views/field/NonTranslatableCustom.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\openculturas_custom\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\Custom;
+
+/**
+ * A handler to provide a field that is completely custom by the administrator.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("non_translatable_custom")
+ */
+class NonTranslatableCustom extends Custom {
+
+}
