63375f4c62e49ce4f8315c1652bfe546ebeda323
tobiasb <tobias.baehr@gzevd.de>
2024-05-15 10:18:35 +0200

    fix: Enable missing media edit button - taxonomy term bundles

--- profile/modules/custom/openculturas_faq/openculturas_faq.post_update.php
+++ profile/modules/custom/openculturas_faq/openculturas_faq.post_update.php
@@ -35,3 +35,23 @@ function openculturas_faq_post_update_enable_media_edit(): void {
   }
 
 }
+
+/**
+ * Enable missing media edit button.
+ */
+function openculturas_faq_post_update_enable_media_edit_2(): void {
+  if (\Drupal::moduleHandler()->moduleExists('media_library_edit') === FALSE) {
+    return;
+  }
+
+  /** @var \Drupal\Core\Entity\EntityDisplayRepositoryInterface $entity_display */
+  $entity_display = \Drupal::service('entity_display.repository');
+  $entityFormDisplay = $entity_display->getFormDisplay('taxonomy_term', 'faq_category');
+  if (!$entityFormDisplay->isNew() && $entityFormDisplay->getComponent('field_mood_image')) {
+    $component = $entityFormDisplay->getComponent('field_mood_image');
+    $component['third_party_settings']['media_library_edit']['show_edit'] = '1';
+    $entityFormDisplay->setComponent('field_mood_image', $component);
+    $entityFormDisplay->save();
+  }
+
+}
