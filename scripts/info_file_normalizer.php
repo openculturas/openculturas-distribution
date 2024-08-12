fad470c9b95694d2c4f52b54539d8fc0745dc291
tobiasb <tobias.baehr@gzevd.de>
2024-08-12 11:41:42 +0200

    chore(release): Release 2.1.1

--- scripts/info_file_normalizer.php
+++ scripts/info_file_normalizer.php
@@ -236,7 +236,7 @@ $comments = new class
 
 $finder = (new Finder())->files()->in('profile/')->exclude('config/')->name('*.info.yml');
 
-const VERSION = '2.1.0';
+const VERSION = '2.1.1';
 foreach ($finder->getIterator() as $file) {
   $yamlContent = $file->getContents();
   $comments->collect(explode("\n", $yamlContent));
