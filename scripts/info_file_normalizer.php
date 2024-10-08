fedab48f08c8265ce43396afb845af986aad92b6
tobiasb <tobias.baehr@gzevd.de>
2024-10-08 12:20:35 +0200

    chore(release): Release 2.3.0-beta1

--- scripts/info_file_normalizer.php
+++ scripts/info_file_normalizer.php
@@ -236,7 +236,7 @@ $comments = new class
 
 $finder = (new Finder())->files()->in('profile/')->exclude('config/')->name('*.info.yml');
 
-const VERSION = '2.2.1';
+const VERSION = '2.3.0-beta1';
 foreach ($finder->getIterator() as $file) {
   $yamlContent = $file->getContents();
   $comments->collect(explode("\n", $yamlContent));
