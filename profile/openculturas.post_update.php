fc915a12181f778bd76c8e0c867d1346bea7cc2e
Moritz Steinhauer <ms@landschaftsverband.org>
2024-11-26 15:00:30 +0100

    feat: user-filter in people-view now also searches in the field display-name
    Co-authored-by: tobiasb <tobias.baehr@gzevd.de>
    Co-authored-by: Meike Jung <meike.jung@cowain.de>

--- profile/openculturas.post_update.php
+++ profile/openculturas.post_update.php
@@ -804,3 +804,27 @@ function openculturas_post_update_search_input_label(): void {
     $view->save();
   }
 }
+
+/**
+ * Revert view 'user_admin_people' to improve name filter.
+ */
+function openculturas_post_update_user_admin_people_add_realname(): string {
+  $full_config_names = [
+    'views.view.user_admin_people',
+  ];
+  /** @var \Drupal\config_update\ConfigReverter $configUpdater */
+  $configUpdater = \Drupal::service('config_update.config_update');
+  /** @var \Drupal\update_helper\UpdateLogger $logger */
+  $logger = \Drupal::service('update_helper.logger');
+  foreach ($full_config_names as $full_config_name) {
+    $config_name = ConfigName::createByFullName($full_config_name);
+    if ($configUpdater->revert($config_name->getType(), $config_name->getName())) {
+      $logger->info(sprintf('Configuration %s has been successfully reverted.', $full_config_name));
+    }
+    else {
+      $logger->warning(sprintf('Unable to import %s config, because configuration file is not found.', $full_config_name));
+    }
+  }
+
+  return $logger->output();
+}
