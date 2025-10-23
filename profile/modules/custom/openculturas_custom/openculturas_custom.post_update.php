2b8a8ca852f37fcdc30bddbf47eb1be482c9b2cb
tobiasb <tobias.baehr@gzevd.de>
2025-10-23 10:31:23 +0200

    chore: Remove all (post) update hooks <= v2.5.0

--- profile/modules/custom/openculturas_custom/openculturas_custom.post_update.php
+++ profile/modules/custom/openculturas_custom/openculturas_custom.post_update.php
@@ -6,9 +6,5 @@ declare(strict_types=1);
  * Implements hook_removed_post_updates().
  */
 function openculturas_custom_removed_post_updates(): array {
-  return [
-    'openculturas_custom_post_update_set_allowed_values_function_for_field_status' => '2.2.0',
-    'openculturas_custom_post_update_set_allowed_values_function_for_field_premiere' => '2.2.0',
-    'openculturas_custom_post_update_grant_administer_openculturas_custom_configuration' => '2.2.0',
-  ];
+  return [];
 }
