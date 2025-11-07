7d5b5a755f70b5fadebb6b1c3fa4899d180f647b
tobiasb <tobias.baehr@gzevd.de>
2025-11-07 16:51:02 +0100

    [#3555875] fix: Revert removal of post updates of 2.5.x

--- profile/modules/custom/openculturas_custom/openculturas_custom.post_update.php
+++ profile/modules/custom/openculturas_custom/openculturas_custom.post_update.php
@@ -6,5 +6,9 @@ declare(strict_types=1);
  * Implements hook_removed_post_updates().
  */
 function openculturas_custom_removed_post_updates(): array {
-  return [];
+  return [
+    'openculturas_custom_post_update_set_allowed_values_function_for_field_status' => '2.2.0',
+    'openculturas_custom_post_update_set_allowed_values_function_for_field_premiere' => '2.2.0',
+    'openculturas_custom_post_update_grant_administer_openculturas_custom_configuration' => '2.2.0',
+  ];
 }
--- profile/modules/custom/openculturas_faq/openculturas_faq.post_update.php
+++ profile/modules/custom/openculturas_faq/openculturas_faq.post_update.php
@@ -6,5 +6,10 @@ declare(strict_types=1);
  * Implements hook_removed_post_updates().
  */
 function openculturas_faq_removed_post_updates(): array {
-  return [];
+  return [
+    'openculturas_faq_post_update_interaction_button_section' => '2.2.0',
+    'openculturas_faq_post_update_enable_media_edit' => '2.2.0',
+    'openculturas_faq_post_update_enable_media_edit_2' => '2.2.0',
+    'openculturas_faq_post_update_source_string_spell_corrections' => '2.2.0',
+  ];
 }
