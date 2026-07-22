1ecd36677eb8ef4748f5e05250ee6fd4f8699772
tobiasb <tobias.baehr@gzevd.de>
2026-07-22 13:29:49 +0200

    refactor(opcult_starterkit): convert to oop-hooks

---
+++ profile/themes/opcult_starterkit/src/Hook/OpcultStarterkitHooks.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\opcult_starterkit\Hook;
+
+/**
+ * Hook implementations for opcult_starterkit.
+ *
+ * This sub-theme inherits all preprocess functions from opcult.
+ * Add your custom hook implementations below, e.g.:
+ *
+ * #[Hook('preprocess_page')]
+ * public function preprocessPage(array &$variables): void {
+ * }
+ */
+class OpcultStarterkitHooks {
+}
