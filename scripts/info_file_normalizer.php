3a5928b91d93dd902102053d5d8e1fce40870393
tobiasb <tobias.baehr@gzevd.de>
2026-08-12 14:25:38 +0200

    chore(release): Prepare 2.6.3

--- scripts/info_file_normalizer.php
+++ scripts/info_file_normalizer.php
@@ -238,7 +238,7 @@ $comments = new class
 
 $finder = (new Finder())->files()->in('profile/')->exclude('config/')->name('*.info.yml');
 
-const VERSION = '2.6.x-dev';
+const VERSION = '2.6.3';
 foreach ($finder->getIterator() as $file) {
   $yamlContent = $file->getContents();
   $comments->collect(explode("\n", $yamlContent));
