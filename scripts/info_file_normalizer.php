fd686ec36ce6df57585452b63bbe7bd6a0f25167
tobiasb <tobias.baehr@gzevd.de>
2026-08-28 11:09:18 +0200

    chore: Back to 3.1.x-dev

--- scripts/info_file_normalizer.php
+++ scripts/info_file_normalizer.php
@@ -238,7 +238,7 @@ $comments = new class
 
 $finder = (new Finder())->files()->in('profile/')->exclude('config/')->name('*.info.yml');
 
-const VERSION = '3.1.0-rc1';
+const VERSION = '3.1.x-dev';
 foreach ($finder->getIterator() as $file) {
   $yamlContent = $file->getContents();
   $comments->collect(explode("\n", $yamlContent));
